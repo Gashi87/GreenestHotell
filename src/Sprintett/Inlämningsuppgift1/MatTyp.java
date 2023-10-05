3db0bd022a223dd5aa7f6c1a70e1191173d52f71
Gashi87 <shayan_koyi@hotmail.com>
2023-10-05 20:07:27 +0200

    Uppdaterar klass Kaktus, andra försök att få med enum och interface

---
+++ src/Sprintett/Inlämningsuppgift1/MatTyp.java
@@ -0,0 +1,12 @@
+package Sprintett.Inlämningsuppgift1;
+
+
+/*Jag har skapat en enum-klass som heter matTyp för att representera de olika typerna av näring eller vätska
+     som våra växtmodeller kan ta upp. Istället för att använda strängar eller heltal, ger detta enum mig en tydlig
+     begränsad uppställning värden jag kan arbeta med.
+     */
+public enum MatTyp {
+    VATTEN,
+    MINERALVATTEN,
+    PROTEINDRYCK
+}
